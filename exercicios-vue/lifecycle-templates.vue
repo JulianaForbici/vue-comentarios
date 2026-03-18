27461e78c92c5224f493b28f99aa566239a2b5fa
juliana.forbici <juliana.forbici@senior.com.br>
2026-03-18 15:49:09 -0300

    first commit

---
+++ exercicios-vue/lifecycle-templates.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const pElementRef = ref(null)
+
+onMounted(() => {
+  pElementRef.value.textContent = 'alo'
+})
+</script>
+<template>
+  <p ref="pElementRef">Hello</p>
+</template>
---
+++ exercicios-vue/reactive.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter=reactive({ count : 0})
+const message = ref('hello world')
+</script>
+
+<template>
+  <h1>
+    <p>Contador: {{ counter.count }}</p>
+    <p>Mensagem: {{ message }}</p>
+  </h1>
+</template>
